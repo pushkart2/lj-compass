71014e65129140fb49067f105cefdb41b89120b3
loljoshie <91661118+loljoshie@users.noreply.github.com>
2021-12-07 16:46:18 -0700

    Fixed type error for lj-menu

--- html/app.js
+++ html/app.js
@@ -61,29 +61,22 @@ window.addEventListener("message", function (event) {
         streetA = event.data.streetA
         value = event.data.value
         streetB = event.data.streetB
-         $(".bar").show();
         if (streetA === null) {
             $(".streetA").hide();
-            $(".compass").hide();
         } else  {
             $('.streetA').html(streetA);
-            $(".streetA").show();
             $('.streetB').html(streetB);
-            $(".streetB").show();
-            $('.compass').html(value);
-            $(".compass").show();
-
-            if (value  !== undefined) {
-                bar = document.getElementsByTagName("svg")[0];
+            if (value) {
+                bar = document.getElementsByClassName("svg")[0];
                 bar.setAttribute("viewBox", ''+ (value - 90) + ' 0 180 5');
-                heading = document.getElementsByTagName("svg")[1];
+                heading = document.getElementsByClassName("svg")[1];
                 heading.setAttribute("viewBox", ''+ (value - 90) + ' 0 180 1.5');
             }
         }
-
+        $(".streetA").fadeIn();
+        $(".streetB").fadeIn();
         $(".ui").fadeIn();
     } else if (event.data.action == "hide") {
-        $(".bar").show();
         $(".streetA").fadeOut();
         $(".streetB").fadeOut();
         $(".ui").fadeOut();
